eaab4a5468d5164a613a89c8a88441ecaf9b3cb1
Anna Royer <anna.royer@colorado.edu>
2018-01-25 14:23:09 -0700

    DEBUG edit feature test error

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -19,19 +19,19 @@ class LittleShopApp < Sinatra::Base
     redirect :"merchants/#{merchant.id}"
   end
 
-  get '/merchants/:id/edit' do
-    @merchant = Merchant.find(params[:id])
-    erb :"merchants/edit"
-  end
-
   get '/merchants/:id' do
     @merchant = Merchant.find(params[:id])
     erb :"merchants/show"
   end
 
-  put '/merchants/:id/update' do |id|
+  get '/merchants/:id/edit' do
+    @merchant = Merchant.find(params[:id])
+    erb :"merchants/edit"
+  end
+
+  put '/merchants/:id' do |id|
     Merchant.update(id.to_i, params[:merchant])
-    redirect :"merchants"
+    redirect :"/merchants/#{id}"
   end
 
 end
--- spec/features/user_can_edit_a_merchant_spec.rb
+++ spec/features/user_can_edit_a_merchant_spec.rb
@@ -1,13 +1,13 @@
 describe "as a user" do
-  describe "when I go to merchant/index/id/edit" do
+  describe "when I go to merchant/id/edit" do
     it "edits an existing merchant" do
       Merchant.create(name: 'Sal')
       visit "merchants/1/edit"
 
       fill_in "merchant[name]", with: "Sal My Savior"
-      click_button "submit"
+      click_button "Submit"
 
-      expect(current_path).to eq("/merchants")
+      expect(current_path).to eq("/merchants/1")
       expect(page).to have_content("Sal My Savior")
     end
   end
