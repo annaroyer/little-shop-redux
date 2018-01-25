74e4fb36f2e60926bf65c379a8976cc779d1cc19
Anna Royer <anna.royer@colorado.edu>
2018-01-25 14:01:44 -0700

    Fix edit feature test error

--- spec/features/user_can_edit_a_merchant_spec.rb
+++ spec/features/user_can_edit_a_merchant_spec.rb
@@ -1,6 +1,7 @@
 describe "as a user" do
   describe "when I go to merchant/index/id/edit" do
     it "edits an existing merchant" do
+      Merchant.create(name: 'Sal')
       visit "merchants/1/edit"
 
       fill_in "merchant[name]", with: "Sal My Savior"
