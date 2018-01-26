a1ae0af3d506ef84b16c6b74fea09851bd4b10fe
Anna Royer <anna.royer@colorado.edu>
2018-01-26 00:09:42 -0700

    Add iteration 6 seeding items functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
 require 'csv'
 require './app/models/merchant'
+require './app/models/item'
 
 
 class Seeds
   CSV.foreach("./db/csv/merchants.csv", headers: true, header_converters: :symbol) do |row|
     Merchant.create!(row.to_hash)
+  end
+
+  CSV.foreach("./db/csv/items.csv", headers: true, header_converters: :symbol) do |row|
+    price = (row[:unit_price].to_f / 100).round(2)
+
+    Item.create!( title: row[:name],
+                  description: row[:description],
+                  price: price,
+                  image: "https://i.pinimg.com/564x/39/f0/5f/39f05f7c2df0d2d5eec301a32c8fb38a.jpg"
+                )
   end
 end
