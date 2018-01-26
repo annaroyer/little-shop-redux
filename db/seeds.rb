cb1b91ce3d7c6e1f6f33228dca6e3f84a79a846a
Anna Royer <anna.royer@colorado.edu>
2018-01-25 17:28:16 -0700

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,7 @@ require 'csv'
 require './app/models/merchant'
 
 class Seeds
-
-  def merchants
-    CSV.foreach("./data/merchants.csv", headers: true, header_converters: :symbol) do |row|
-      Merchant.create!(merchant_id: row[:id], name: row[:name], merchant_created_at: row[:created_at], merchant_updated_at: row[:updated_at])
-    end
+  CSV.foreach("./data/merchants.csv", headers: true, header_converters: :symbol) do |row|
+    Merchant.create!(row.to_hash)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ Bundler.require(:default, :test)
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
 require 'capybara/dsl'
+require 'capybara/rspec'
 
 Capybara.app = LittleShopApp
 
