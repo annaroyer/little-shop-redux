20cb471b96739632ad8a8be25cbad5a6fdffcd06
Luis Garcia <garcia@yahoo.com>
2018-01-26 00:26:02 -0700

    Complete crud functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
 require 'csv'
 require './app/models/merchant'
+require './app/models/category'
 
 
-class Seeds
-  CSV.foreach("./db/csv/merchants.csv", headers: true, header_converters: :symbol) do |row|
-    Merchant.create!(row.to_hash)
+class Seed
+
+  OPTIONS = {headers: true, header_converters: :symbol}
+
+  def self.start
+    seed_merchants
+    seed_categories
   end
+
+  def self.seed_merchants
+    CSV.foreach("./db/csv/merchants.csv", OPTIONS) do |row|
+      Merchant.create!(row.to_hash)
+    end
+  end
+
+  def self.seed_categories
+    CSV.foreach("./db/csv/category.csv", OPTIONS) do |row|
+      Category.create!(row.to_hash)
+    end
+  end
+
 end
+
+Seed.start
