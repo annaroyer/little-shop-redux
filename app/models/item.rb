257adf80b1945ae3e27aa697b200a7521805c35a
Anna Royer <anna.royer@colorado.edu>
2018-01-27 15:08:34 -0700

    Add merchant_id and category_id to merchants and create database relationships in models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ActiveRecord::Base
   validates_presence_of :title, :description, :unit_price, :image
+  belongs_to :merchant, :category
 
   def price
-    (self.unit_price / 100).round(2)    
+    (self.unit_price / 100).round(2)
   end
 end
