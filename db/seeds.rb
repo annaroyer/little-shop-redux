31cfc59ef9e6d63842d398c9d9d9ef8b61148fbc
Anna Royer <anna.royer@colorado.edu>
2018-01-27 16:56:55 -0700

    Include merchant ids in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,12 @@ class Seed
   end
 
   CSV.foreach("./db/csv/items.csv", OPTIONS) do |row|
+    require 'pry'; binding.pry
     Item.create!( title:       row[:name],
                   description: row[:description],
                   unit_price:  row[:unit_price],
-                  image:       "https://i.pinimg.com/564x/39/f0/5f/39f05f7c2df0d2d5eec301a32c8fb38a.jpg"
+                  image:       "https://i.pinimg.com/564x/39/f0/5f/39f05f7c2df0d2d5eec301a32c8fb38a.jpg",
+                  merchant_id: row[:merchant_id]
                 )
   end
 
