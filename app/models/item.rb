bd8548463a55f1e4524f7ce7fb4ff2766534106f
Anna Royer <anna.royer@colorado.edu>
2018-01-27 15:40:27 -0700

    fix belongs_to in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
   validates_presence_of :title, :description, :unit_price, :image
-  belongs_to :merchant, :category
+  belongs_to :merchant
+  belongs_to :category
 
   def price
     (self.unit_price / 100).round(2)
