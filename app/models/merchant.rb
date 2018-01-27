96410804913935fc29405472cd26582df781372e
Luke Chambers <lukenatchambers@gmail.com>
2018-01-26 19:07:39 -0700

    Build all tests for iteration 9.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,17 @@
 class Merchant < ActiveRecord::Base
   validates_presence_of :name
+
+  has_many :items
+
+  def self.highest_price_item
+    items.order("unit_price DESC").first
+  end
+
+  def self.how_many_items?
+    items.count
+  end
+
+  def self.total_price_of_items
+    items.sum("unit_price")
+  end
 end
