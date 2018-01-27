a3dfff7cf35c0d181b2e9a2b32a9bb43f0fcdfad
Anna Royer <anna.royer@colorado.edu>
2018-01-27 14:24:57 -0700

    fix price to format with 2 decimals

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ActiveRecord::Base
   validates_presence_of :title, :description, :unit_price, :image
 
   def price
-    (self.unit_price.to_f / 100).round(2)
+    '%.2f' % (self.unit_price / 100).round(2)    
   end
 end
