c2e1a52bdfa372399593e4ac3599589eefe187ee
Anna Royer <anna.royer@colorado.edu>
2018-01-27 14:38:16 -0700

    Move price string formatter from item model to views for item index and item show

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ActiveRecord::Base
   validates_presence_of :title, :description, :unit_price, :image
 
   def price
-    '%.2f' % (self.unit_price / 100).round(2)    
+    (self.unit_price / 100).round(2)    
   end
 end
