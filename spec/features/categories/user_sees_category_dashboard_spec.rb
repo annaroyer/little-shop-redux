0a7e0a9062add54d8d9355b2feb7d9c664305ba5
Anna Royer <anna.royer@colorado.edu>
2018-01-28 01:05:07 -0700

    Add feature tests for category with most and least expensive items

--- spec/features/categories/user_sees_category_dashboard_spec.rb
+++ spec/features/categories/user_sees_category_dashboard_spec.rb
@@ -21,11 +21,40 @@ describe "As a user" do
     end
 
     it "I can see the category with the most expensive item" do
+      category_1 = Category.create(name: "clothing")
+      category_1.items.create(title: "dress", description: "vintage", unit_price: 25000, image: "dress.jpg")
+      category_1.items.create(title: "tshirt", description: "xxs", unit_price: 3500, image: "girl-in-tshirt/shirt.jpg")
 
+      category_2 = Category.create(name: "technology")
+      category_2.items.create(title: "cell phone", description: "iphone x", unit_price: 99900, image: "iphone-x.jpg")
+      category_2.items.create(title: "survival flashlight", description: "multiple modes", unit_price: 4000, image: "best-survival-flashlight.jpg")
+
+      category_3 = Category.create(name: "crafts")
+      category_3.items.create(title: "glitter", description: "dangerous", unit_price: 150, image: "sparklesplosion.jpg")
+      category_3.items.create(title: "felt", description: "fuzzy", unit_price: 250, image: "piece-of-felt.jpg")
+
+      visit '/categories-dashboard'
+
+      expect(page).to have_content("Category with Most Expensive Item: technology")
     end
 
     it "I can see the category with the least expensive item" do
 
+        category_1 = Category.create(name: "clothing")
+        category_1.items.create(title: "dress", description: "vintage", unit_price: 25000, image: "dress.jpg")
+        category_1.items.create(title: "tshirt", description: "xxs", unit_price: 3500, image: "girl-in-tshirt/shirt.jpg")
+
+        category_2 = Category.create(name: "technology")
+        category_2.items.create(title: "cell phone", description: "iphone x", unit_price: 99900, image: "iphone-x.jpg")
+        category_2.items.create(title: "survival flashlight", description: "multiple modes", unit_price: 4000, image: "best-survival-flashlight.jpg")
+
+        category_3 = Category.create(name: "crafts")
+        category_3.items.create(title: "glitter", description: "dangerous", unit_price: 150, image: "sparklesplosion.jpg")
+        category_3.items.create(title: "felt", description: "fuzzy", unit_price: 250, image: "piece-of-felt.jpg")
+
+        visit '/categories-dashboard'
+
+        expect(page).to have_content("Category with Least Expensive Item: crafts")
     end
   end
 end
