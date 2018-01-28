4a80c90d2a9e0fc114c6f960ab1e4f5bc98c8ba5
Luis Garcia <garcia@yahoo.com>
2018-01-28 16:26:54 -0700

    Create it7 model methods

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,37 +1,48 @@
 describe Item do
-  describe 'Validations' do
-    it 'is invalid without a title' do
-      item = Item.create(description: 'soft and fluffy', unit_price: 3500, image: "http://tinypuppy.png")
+  describe "Validations" do
+    it "is invalid without a title" do
+      item = Item.create(description: "soft and fluffy", unit_price: 3500, image: "http://tinypuppy.png")
 
       expect(item).to be_invalid
     end
 
-    it 'is invalid without a description' do
-      item = Item.create(title: 'puppy', unit_price: 3500, image: "http://tinypuppy.png")
+    it "is invalid without a description" do
+      item = Item.create(title: "puppy", unit_price: 3500, image: "http://tinypuppy.png")
 
       expect(item).to be_invalid
     end
 
-    it 'is invalid without a price' do
-      item = Item.create(title: 'puppy', description: 'soft and fluffy', image: "http://tinypuppy.png")
+    it "is invalid without a price" do
+      item = Item.create(title: "puppy", description: "soft and fluffy", image: "http://tinypuppy.png")
 
       expect(item).to be_invalid
     end
 
-    it 'is invalid without an image' do
-      item = Item.create(title: 'puppy', description: 'soft and fluffy', unit_price: 3500)
+    it "is invalid without an image" do
+      item = Item.create(title: "puppy", description: "soft and fluffy", unit_price: 3500)
 
       expect(item).to be_invalid
     end
   end
 
-  describe 'Instance Methods' do
-    describe '#price' do
-      it 'returns an price in dollars as a decimal' do
-        item = Item.create(title: 'puppy', description: 'soft and fluffy', unit_price: 3599, image: "http://tinypuppy.png")
+  describe "Instance Methods" do
+    describe "#price" do
+      it "returns an price in dollars as a decimal" do
+        item = Item.create(title: "puppy", description: "soft and fluffy", unit_price: 3599, image: "http://tinypuppy.png")
 
         expect(item.price).to eq(35.99)
       end
     end
   end
+
+  describe "Class Methods" do
+    describe ".total_item_count" do
+      it "returns total item count" do
+        item_1 = Item.create(title: "dress", description: "vintage", unit_price: 25000, image: "dress.jpg")
+        item_2 = Item.create(title: "shirt", description: "small", unit_price: 25000, image: "dress.jpg")
+        item_3 = Item.create(title: "pantalon", description: "viejo", unit_price: 25000, image: "dress.jpg")
+      end
+    end
+  end
+
 end
