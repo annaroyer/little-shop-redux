7495a9713d0b2c49af7b70f7bad6b25f532af7df
Anna Royer <anna.royer@colorado.edu>
2018-01-28 21:50:47 -0700

    refactor category with most and least expensive item class methods to be solely active record

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,16 +2,18 @@ class Category < ActiveRecord::Base
   validates_presence_of :name
   has_many :items
 
+  def self.sort_by_item_price
+    select("categories.*, items.*")
+      .joins(:items)
+      .order("unit_price DESC")
+  end
+
   def self.most_expensive_item
-    Category.all.max_by do |category|
-      category.items.maximum(:unit_price)
-    end
+    sort_by_item_price.first
   end
 
   def self.least_expensive_item
-    Category.all.min_by do |category|
-      category.items.minimum(:unit_price)
-    end
+    sort_by_item_price.last
   end
 
   def average_item_price
