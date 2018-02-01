16e49c09942bfcd25ac343e320ee2af98843c763
Anna Royer <anna.royer@colorado.edu>
2018-01-31 22:55:11 -0700

    work on feature tests

--- spec/features/items/user_can_create_a_single_item_spec.rb
+++ spec/features/items/user_can_create_a_single_item_spec.rb
@@ -1,12 +1,18 @@
 describe "As a user" do
   describe "when I go to the item index page" do
     it "I can create a new item" do
+      Merchant.create(name: 'cutesockz4987')
+
       visit '/items/new'
 
       fill_in "item[title]", with: "puppy"
       fill_in "item[description]", with: "soft and fluffy"
       fill_in "item[unit_price]", with: 35.00
       fill_in "item[image]", with: "http://tinypuppy.png"
+      within first('optgroup') do
+        select("cutesockz4987")
+      end
+
       click_button "Create"
 
       expect(current_path).to eq("/items/1")
